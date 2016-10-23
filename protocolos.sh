fe8c3bad98aa3a49e177da2e213d4805f00bac43
dacarpe <dacarpe@gmail.com>
2016-10-23 19:57:42 -0200

    Update protocolos.sh

--- protocolos.sh
+++ protocolos.sh
@@ -6,13 +6,30 @@ LRED='\e[1;31m'; # Light Red
 LGREEN='\e[1;32m'; #Light Green
 NC='\e[0m'; #No Color
 
-# Tela de apresentação do script
-clear
-figlet Protocolos Net
-echo "Versão 1.1 - Autor: Daniel Carvalho"
+# DECLARANDO FUNÇÕES
+# Função para agrupar os dados
+imprimindo_dados(){
+echo "DIA / HORA: $DATA"
+echo "ATENDENTE: $ATEND"
+echo "PROTOCOLO: $PROTO"
+echo "DESCRIÇÃO: $DESCR"
 echo
 echo
+}
 
+procurando_data(){
+echo "Insira os dados a serem procurados"
+echo "(Deixe em branco caso queira pular para outro campo)"
+echo
+echo -n "DATA [dd-mm-aa]: "
+read $DATA_PROC
+echo
+echo "Exibindo dados requisitados abaixo:"
+echo
+cat protocolos_BD.txt | grep -A 10 "$DATA_PROC"
+}
+
+inserindo_dados(){
 while [ "$CONTINUA" != "n" ]
 do
 	# Interação inicial com o usuário
@@ -29,24 +46,43 @@ do
 	echo -n -e "${LRED}DESCRIÇÃO: ${NC}"
 	read DESCR
 	
-	# Função para agrupar os dados
-	dados(){
-	echo "DIA / HORA: $DATA"
-	echo "ATENDENTE: $ATEND"
-	echo "PROTOCOLO: $PROTO"
-	echo "DESCRIÇÃO: $DESCR"
-	echo
-	echo
-	}
-	
 	# Chamando a função e jogando o resultado para um arquivo de log
-	dados >> protocolos1.0.txt
+	imprimindo_dados >> protocolos_BD.txt
 
 	# Vamos terminar ou fazer de novo?
+	echo
 	echo -n -e "${LGREEN}Você quer inserir outro protocolo?${NC} [s/n] "
 	read CONTINUA
 done
-	
-# Terminando o script
+}
+
+menu(){
 clear
-exit
+figlet Protocolos Net
+echo "Versão 1.2 - Autor: Daniel Carvalho"
+echo
+echo
+echo "O que você deseja fazer?"
+echo
+echo "[1] Inserir um protocolo"
+echo "[2] Procurar um protocolo por data"
+echo "[3] Sair"
+echo
+echo -n -e "${LGREEN}Opção:${NC} "
+read OPCAO
+
+# Fazendo as opções escolhidas acontecerem
+case $OPCAO in
+	1) inserindo_dados ;;
+	2) procurando_data ;;
+	3) echo ;
+		 exit ;;
+	*) echo ;
+		echo -e "${LRED}Opção Inexistente${NC}";
+		sleep 2 ;
+		clear ;
+	menu ;;
+esac
+}
+
+menu
